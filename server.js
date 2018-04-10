6d3da73c2fedc9f3683917f48e24bebed55b9eb6
e30devo <dthomase30@gmail.com>
2018-04-10 12:42:33 -0700

    Configured for Heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var db = require("./models");
 
 var app = express();
@@ -12,10 +12,10 @@ var app = express();
 app.use(express.static("public"))
 require("./routes/api-routes.js")(app);
 
+const MONGODB_URI = (process.env.MONGODB_URI || "mongodb://localhost/scraped-up")
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URI)
+
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
 });
-
-mongoose.connect("mongodb://localhost/scraped-up");
-
-
